5403f212c433559aa8bc5355161e9d10ebda2e7a
darciso@me.com <darciso@me.com>
2020-09-04 09:11:04 -0300

    Ajustes no edpoint /categorias/{id}

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -23,8 +23,12 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
-		return ResponseEntity.ok().body(obj);
+	
+			Categoria obj = service.buscar(id);
+			return ResponseEntity.ok().body(obj);
+
+			
+
 		
 
 	}
